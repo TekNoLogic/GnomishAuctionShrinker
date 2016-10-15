cf0145fa1098a1ec0848dc3778f624ad334cca78
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-10-14 18:25:44 -0600

    Move header enable/disable logic into the right file

--- frames/header_button.lua
+++ frames/header_button.lua
@@ -8,6 +8,7 @@ local function OnClick(self)
 end
 
 
+local buttons = {}
 function ns.CreateHeaderButton(text, column)
 	local butt = ns.NewColumnHeader(AuctionFrameBrowse)
 	butt:SetText(text)
@@ -15,5 +16,29 @@ function ns.CreateHeaderButton(text, column)
 	columns[butt] = column
 	if column then butt:SetScript("OnClick", OnClick) end
 
+	buttons[butt] = true
+
 	return butt
 end
+
+
+local all_scan
+local enabler = CreateFrame("Frame")
+enabler:Hide()
+
+enabler:SetScript("OnShow", function(self)
+	for butt in pairs(buttons) do butt:Disable() end
+	if all_scan then self:Hide() end
+end)
+
+enabler:SetScript("OnUpdate", function(self)
+	if not CanSendAuctionQuery("list") then return end
+
+	for butt in pairs(buttons) do butt:Enable() end
+	self:Hide()
+end)
+
+hooksecurefunc("QueryAuctionItems", function(_, _, _, _, _, _, get_all)
+	all_scan = get_all
+	enabler:Show()
+end)
