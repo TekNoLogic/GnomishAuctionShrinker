4a926054e7719a16ad2e24a5e1c348050337ae19
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-10-17 22:22:31 -0600

    Track the selection in its service

--- services/selected_auction.lua
+++ services/selected_auction.lua
@@ -2,17 +2,26 @@
 local myname, ns = ...
 
 
+local selected
+
+
+function ns.GetSelectedAuction()
+	return selected
+end
+
+
 hooksecurefunc("SetSelectedAuctionItem", function(type, index)
 	if type ~= "list" then return end
+	selected = index
 	ns.SendMessage("SELECTION_CHANGED", index)
 end)
 
 
-ns.RegisterCallback({}, "AUCTION_QUERY_SENT", function()
+local function ClearSelection()
+	selected = nil
 	ns.SendMessage("SELECTION_CHANGED", nil)
-end)
+end
 
 
-hooksecurefunc("PlaceAuctionBid", function()
-	ns.SendMessage("SELECTION_CHANGED", nil)
-end)
+ns.RegisterCallback({}, "AUCTION_QUERY_SENT", ClearSelection)
+hooksecurefunc("PlaceAuctionBid", ClearSelection)
