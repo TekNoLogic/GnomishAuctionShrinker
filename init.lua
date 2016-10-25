1f24fbf805fc8e6aeb3501bad2df173b3e1c1548
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-10-25 03:31:29 -0600

    Fix init

--- init.lua
+++ init.lua
@@ -2,10 +2,10 @@
 local myname, ns = ...
 
 
-function ns.OnLoad()
+ns.RegisterCallback({}, "_THIS_ADDON_LOADED", function()
   ns.CreatePanel()
-  
+
   for i,v in pairs(ns) do
     if i:match("^Create") then ns[i] = nil end
   end
-end
+end)
