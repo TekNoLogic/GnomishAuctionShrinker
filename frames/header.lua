cb0f2f22a0983a5c8461037741623c06140c0b42
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-10-30 16:59:45 -0600

    Clean up header construstor

--- frames/header.lua
+++ frames/header.lua
@@ -2,19 +2,6 @@
 local myname, ns = ...
 
 
-local BUYOUT_LIMIT = 800 * 100 * 100 -- 800g
-local NUM_ROWS, BOTTOM_GAP = 14, 25
-local ROW_HEIGHT = math.floor((305-BOTTOM_GAP)/NUM_ROWS)
-
-
-local function AnchorSort(butt, column, row)
-	butt:SetPoint("TOP", row)
-	butt:SetPoint("BOTTOM", row)
-	butt:SetPoint("LEFT", column)
-	butt:SetPoint("RIGHT", column)
-end
-
-
 function ns.CreateHeader(parent, columns)
 	local header = CreateFrame("Frame", nil, parent)
 	header:SetHeight(19)
@@ -22,38 +9,25 @@ function ns.CreateHeader(parent, columns)
 	header:SetPoint("RIGHT")
 
 
-	local qualitysort = ns.CreateHeaderButton(header, "Item", "quality")
-	AnchorSort(qualitysort, columns[1], header)
+	local i = 1
+	local function Make(name, sort)
+		local butt = ns.CreateHeaderButton(header, name, sort)
 
+		butt:SetPoint("TOP", header)
+		butt:SetPoint("BOTTOM", header)
+		butt:SetPoint("LEFT", columns[i])
+		butt:SetPoint("RIGHT", columns[i])
 
-	local levelsort = ns.CreateHeaderButton(header, "Lvl", "level")
-	AnchorSort(levelsort, columns[2], header)
+		i = i + 1
+	end
 
-
-	local ilvlsort = ns.CreateHeaderButton(header, "iLvl")
-	AnchorSort(ilvlsort, columns[3], header)
-
-
-	local sellersort = ns.CreateHeaderButton(header, "Seller", "seller")
-	AnchorSort(sellersort, columns[4], header)
-
-
-	local durationsort = ns.CreateHeaderButton(header, "Time", "duration")
-	AnchorSort(durationsort, columns[5], header)
-
-
-	local bidsort = ns.CreateHeaderButton(header, "Bid")
-	AnchorSort(bidsort, columns[6], header)
-
-
-	local buyoutsort = ns.CreateHeaderButton(header, "Buyout", "buyout")
-	AnchorSort(buyoutsort, columns[7], header)
-
-
-	local unitsort = ns.CreateHeaderButton(header, "Unit BO", "unitprice")
-	AnchorSort(unitsort, columns[8], header)
-
-
-	local qtysort = ns.CreateHeaderButton(header, "#", "quantity")
-	AnchorSort(qtysort, columns[9], header)
+	Make("Item", "quality")
+	Make("Lvl", "level")
+	Make("iLvl")
+	Make("Seller", "seller")
+	Make("Time", "duration")
+	Make("Bid")
+	Make("Buyout", "buyout")
+	Make("Unit BO", "unitprice")
+	Make("#", "quantity")
 end
